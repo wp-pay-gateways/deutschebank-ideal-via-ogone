2bdda7ddc6e4ca8e3f1645ea819a4bb3d9b74d78
Remco Tolsma <info@remcotolsma.nl>
2015-11-11 13:51:36 +0100

    Simplified integration class.

--- src/Integration.php
+++ src/Integration.php
@@ -1,8 +1,10 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_DeutscheBank_IDealViaOgone_GatewayIntegration {
+class Pronamic_WP_Pay_Gateways_DeutscheBank_IDealViaOgone_Integration {
 	public function __construct() {
-		$this->id = 'deutschebank-ideal-via-ogone';
+		$this->id       = 'deutschebank-ideal-via-ogone';
+		$this->name     = 'Deutsche Bank - iDEAL via Ogone';
+		$this->provider = 'deutschebank';
 	}
 
 	public function get_config_factory_class() {
