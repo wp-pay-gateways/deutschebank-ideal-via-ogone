edd09170749c0b220578ca67ce29e4d9f7935b65
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 11:04:33 +0100

    Extend from Ogone library.

--- src/Integration.php
+++ src/Integration.php
@@ -1,21 +1,9 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_DeutscheBank_IDealViaOgone_Integration {
+class Pronamic_WP_Pay_Gateways_DeutscheBank_IDealViaOgone_Integration extends Pronamic_WP_Pay_Gateways_Ogone_OrderStandardEasy_Integration {
 	public function __construct() {
 		$this->id       = 'deutschebank-ideal-via-ogone';
 		$this->name     = 'Deutsche Bank - iDEAL via Ogone';
 		$this->provider = 'deutschebank';
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandardEasy_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandardEasy_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandardEasy_Gateway';
 	}
 }
