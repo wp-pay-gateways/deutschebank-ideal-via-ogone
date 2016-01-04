77e5923b594414965e9c4afd9e4ae5368267fc80
Remco Tolsma <info@remcotolsma.nl>
2016-01-04 13:39:29 +0100

    Added tests.

--- src/Integration.php
+++ src/Integration.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Title: Deutsche Bank - iDEAL via Ogone - Integration
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2016
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
 class Pronamic_WP_Pay_Gateways_DeutscheBank_IDealViaOgone_Integration extends Pronamic_WP_Pay_Gateways_Ogone_OrderStandardEasy_Integration {
 	public function __construct() {
 		$this->id       = 'deutschebank-ideal-via-ogone';
