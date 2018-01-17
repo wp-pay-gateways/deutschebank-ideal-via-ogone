0bca0e27bce5b6fef1948fe9339dd0ec693c3e13
Reüel van der Steege <reuel@pronamic.nl>
2018-01-17 11:57:44 +0100

    Happy 2018.

--- src/Integration.php
+++ src/Integration.php
@@ -3,7 +3,7 @@
 /**
  * Title: Deutsche Bank - iDEAL via Ogone - Integration
  * Description:
- * Copyright: Copyright (c) 2005 - 2016
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @author Remco Tolsma
